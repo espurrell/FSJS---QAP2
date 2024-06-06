c3f1f5d46b024190e2888d490725bcc801e816b0
espurrell <150044207+espurrell@users.noreply.github.com>
2024-06-06 20:17:46 -0230

    Task#3 commit

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const EventEmitter = require('events');
+
+// Defn EventEmitter class
+class MyEmitter extends EventEmitter {};
+
+// Init emitter object
+const myEmitter = new MyEmitter();
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -23,7 +30,7 @@ const server = http.createServer((req, res) => {
       filePath += '/index.html';
       break;
     default:
-      filePath += '/404.html'; 
+      filePath += '/404.html';
   }
 
   fs.readFile(filePath, (err, data) => {
@@ -31,12 +38,36 @@ const server = http.createServer((req, res) => {
       res.writeHead(500, { 'Content-Type': 'text/html' });
       res.write('<h1>Server Error</h1>');
       res.end();
+
+      // Emit error event
+      myEmitter.emit('error', '500 Internal Server Error');
     } else {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
+
+      // Emit success event
+      myEmitter.emit('fileRead', filePath);
     }
   });
+
+  // Emit event for route access
+  if (url !== '/') {
+    myEmitter.emit('routeAccessed', url);
+  }
+});
+
+// Listen 
+myEmitter.on('error', (message) => {
+  console.log(`Error Event: ${message}`);
+});
+
+myEmitter.on('fileRead', (filePath) => {
+  console.log(`File Read Event: Successfully read file at ${filePath}`);
+});
+
+myEmitter.on('routeAccessed', (route) => {
+  console.log(`Route Accessed Event: ${route} was accessed`);
 });
 
 server.listen(3000, () => {
